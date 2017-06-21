bc42a91d2a21616542651bdcdd6f204923835a56
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-06-21 10:42:53 +0700

    Add random string when user register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'activation_token' => str_random(64),
         ]);
     }
 }
